0b2b474ed835e9ac38abf9a34cc8da5e818198a7
kinetikeith <khman32@protonmail.com>
2025-03-11 18:41:39 -0400

    Added chroma.js color

--- src/components/ColorPicker.tsx
+++ src/components/ColorPicker.tsx
@@ -1,11 +1,14 @@
-import { useState, useCallback } from "react";
+import { useState, useCallback, useEffect } from "react";
 import { useBBox } from "@/utils/useBBox";
 import { Range } from "react-range";
+import { Button, Input } from "@headlessui/react";
+import { DocumentDuplicateIcon } from "@heroicons/react/20/solid";
+import chroma, { type Color } from "chroma-js";
 
 const degToRad = Math.PI / 180;
 
 function hsToPositionPolar(hue: number, sat: number) {
-  const hueRad = (hue - 90) * degToRad;
+  const hueRad = ((isNaN(hue) ? 0 : hue) - 90) * degToRad;
 
   const x = (Math.cos(hueRad) * sat - 1) * -0.5;
   const y = (Math.sin(hueRad) * sat - 1) * -0.5;
@@ -17,7 +20,8 @@ function positionPolarToHS(x: number, y: number) {
   const xBi = x * -2 + 1;
   const yBi = y * -2 + 1;
 
-  const hue = Math.atan2(yBi, xBi) / degToRad + 90;
+  let hue = Math.atan2(yBi, xBi) / degToRad + 90;
+  if (isNaN(hue)) hue = 0;
   const sat = Math.min(Math.sqrt(yBi * yBi + xBi * xBi), 1);
 
   return [hue, sat];
@@ -25,9 +29,19 @@ function positionPolarToHS(x: number, y: number) {
 
 export default function ColorPicker() {
   const { ref: radarRef, bBox, handleResize } = useBBox<HTMLDivElement>();
+  const [color, setColor] = useState<Color>(chroma("black"));
+
   const [hue, setHue] = useState(0.0);
   const [sat, setSat] = useState(0.0);
-  const [lit, setLit] = useState(0.0);
+  const [val, setVal] = useState(0.0);
+
+  useEffect(() => {
+    const [hue, sat, val] = color.hsv();
+    if (!isNaN(hue) && sat != 0) setHue(hue);
+    if (val != 0) setSat(sat);
+    setVal(val);
+  }, [color]);
+
   const [x, y] = hsToPositionPolar(hue, sat);
   const left = x * bBox.width;
   const top = y * bBox.height;
@@ -39,12 +53,13 @@ export default function ColorPicker() {
         const y = (event.clientY - bBox.y) / bBox.height;
 
         const [hue, sat] = positionPolarToHS(x, y);
+        setColor(chroma.hsv(hue, sat, val));
         setHue(hue);
         setSat(sat);
       }
       event.preventDefault();
     },
-    [bBox, radarRef],
+    [bBox, val, radarRef],
   );
 
   const handleMouseUp = useCallback(() => {
@@ -62,7 +77,8 @@ export default function ColorPicker() {
   );
 
   // TODO: memoize some colors/gradient calculations
-  const backgroundColor = `hsl(${hue}deg ${sat * 100}% ${lit * 100}%)`;
+  const chipColor = color.hex();
+  const maxValColor = chroma.hsv(hue, sat, 1.0).hex();
 
   return (
     <div className="p-4 pt-2 bg-neutral-200 rounded-md shadow-lg flex flex-col items-center space-y-3 w-48">
@@ -74,7 +90,7 @@ export default function ColorPicker() {
       >
         <div className="relative w-0 h-0" style={{ left, top }}>
           <div
-            style={{ backgroundColor }}
+            style={{ backgroundColor: chipColor }}
             className="relative -top-2.5 -left-2.5 size-5 aspect-square rounded-full border-neutral-100 border-4 outline-neutral-400 outline-1 hover:outline-4 hover:outline-neutral-400/50"
           />
         </div>
@@ -83,14 +99,14 @@ export default function ColorPicker() {
         step={0.01}
         min={0}
         max={1}
-        values={[lit]}
-        onChange={(values) => setLit(values[0])}
+        values={[val]}
+        onChange={(values) => setColor(chroma.hsv(hue, sat, values[0]))}
         renderTrack={({ props, children }) => (
           <div
             {...props}
             className="mt-2 h-2 w-full rounded-full"
             style={{
-              background: `linear-gradient(to right, hsl(${hue}deg ${sat * 100}% 0%), hsl(${hue}deg ${sat * 100}% 50%), hsl(${hue}deg ${sat * 100}% 100%)`,
+              background: `linear-gradient(to right, #000000, ${maxValColor})`,
               ...props.style,
             }}
           >
@@ -100,11 +116,27 @@ export default function ColorPicker() {
         renderThumb={({ props }) => (
           <div
             {...props}
-            style={{ backgroundColor, ...props.style }}
+            key={props.key}
+            style={{ backgroundColor: chipColor, ...props.style }}
             className="relative size-5 aspect-square rounded-full border-neutral-100 border-4 outline-neutral-400 outline-1 hover:outline-4 hover:outline-neutral-400/50"
           />
         )}
       />
+      <div className="flex flex-row justify-end mt-4 shadow-sm self-end rounded-sm">
+        <Input
+          type="text"
+          name="hex"
+          placeholder="HEX"
+          className="text-sm font-mono bg-white rounded-l-sm px-1 py-0.5 w-[4.5em]"
+          value={color.hex()}
+        />
+        <Button
+          type="button"
+          className="bg-white text-neutral-700 px-1 border-l-1 border-neutral-300 rounded-r-sm cursor-pointer hover:bg-neutral-200"
+        >
+          <DocumentDuplicateIcon className="size-4" />
+        </Button>
+      </div>
     </div>
   );
 }
